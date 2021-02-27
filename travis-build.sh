03417f7598ecc5e02cbc630a4aa1a8715c32230f
Uri Herrera <hello@uriherrera.com>
2021-02-26 18:29:47 -0600

    create pkg

--- travis-build.sh
+++ travis-build.sh
@@ -10,8 +10,8 @@ git_commit=$(git rev-parse --short HEAD)
 	"Section: misc" \
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
-	"Package: nx-desktop-apps-minimal" \
-	"Version: 0.0.2-$git_commit" \
+	"Package: nx-desktop-apps-legacy-minimal" \
+	"Version: 0.0.1-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
 	"Conflicts: nx-desktop-apps-legacy, nx-desktop-apps" \
