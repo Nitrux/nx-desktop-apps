8b10760b7ab6dcd368c0f81ae8d4f5e548c6a32b
UriHerrera <hello@uriherrera.com>
2021-12-29 20:46:23 -0600

    create pkg

--- travis-build.sh
+++ travis-build.sh
@@ -10,11 +10,10 @@ git_commit=$(git rev-parse --short HEAD)
 	"Section: misc" \
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
-	"Package: nx-desktop-apps-minimal" \
-	"Version: 0.0.6-$git_commit" \
+	"Package: nx-desktop-apps-vfio" \
+	"Version: 0.0.1-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
-	"Conflicts: nx-desktop-apps-legacy, nx-desktop-apps" \
 	"Architecture: amd64" \
 	"Description: NX Desktop Apps metapackage for Nitrux."
 
